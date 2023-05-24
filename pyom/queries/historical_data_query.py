852b0735f1d335285fb11a19cc259e7ab9b1ff52
wegar <artur.wegrzyn@axpo.pl>
2023-05-24 19:48:25 +0200

    Added class DatesInterval for representation of dates interval and completed class HistoricalDataQuery for getting historical data

--- pyom/queries/historical_data_query.py
+++ pyom/queries/historical_data_query.py
@@ -1,9 +1,10 @@
 from pyom.parameters.gps_position import GpsPosition
 from pyom.parameters.meteo_variable import MeteoVariable
+from pyom.parameters.dates_interval import DatesInterval
 
 
 class HistoricalDataQuery:
     gps_position: GpsPosition
     meteo_variable: MeteoVariable
-
+    DatesInterval: DatesInterval
 
