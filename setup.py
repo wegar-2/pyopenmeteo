da8b9dad1488dafe68d552ca1352fc09b3b28b8b
wegar <artur.wegrzyn@axpo.pl>
2023-05-26 20:55:31 +0200

    updated setup.py ,  added requirements file

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="pyom",
     version="0.0.1",
     description="Package for working with OpenMeteo API in Python",
-    author="Artur Wegrzyn"
+    author="Artur Wegrzyn",
+    packages=find_packages(),
+    install_requires="requirements.txt"
 )
 
